e658a4156249113eec22804dbc92427141faeee5
Yonatan <yonatald@gmail.com>
2020-02-19 09:59:34 +0200

    enhancments
    Signed-off-by: Yonatan <yonatald@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,5 @@ const dbName : string = process.env.VIP_DB_NAME || 'vipDB';
 
 export const mongoConnectionString : string =
   process.env.VIP_CONN_STRING || `mongodb://${mongoHost}:27017/${dbName}`;
+
+export const port = process.env.VIP_PROT || '8080';
