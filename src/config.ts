72c31cb0a6b47b5d0398a33b85b2beaba05b55a1
Yonatan <yonatald@gmail.com>
2020-02-19 10:15:11 +0200

    fix: env vriable name
    Signed-off-by: Yonatan <yonatald@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@ const dbName : string = process.env.VIP_DB_NAME || 'vipDB';
 export const mongoConnectionString : string =
   process.env.VIP_CONN_STRING || `mongodb://${mongoHost}:27017/${dbName}`;
 
-export const port = process.env.VIP_PROT || '8080';
+export const port = process.env.VIP_PORT || '8080';
